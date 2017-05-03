79d41f50a3017a433238898bf3d9b49f05880af8
Trevor Santarra <trevor.santarra@gmail.com>
2017-05-03 10:06:39 -0700

    Implementing new test domain.

--- domains/multi_agent/recipe_sat/recipe_sat_scenario.py
+++ domains/multi_agent/recipe_sat/recipe_sat_scenario.py
@@ -1,127 +1,78 @@
-from collections import Counter, namedtuple
-from collections import defaultdict
+from collections import namedtuple
 
-from mdp.scenario import Scenario
 from mdp.distribution import Distribution
 from mdp.state import State
 from mdp.action import JointActionSpace
 
-RecipeAction = namedtuple('RecipeAction', ['act', 'object'])
-RecipeAction.__new__.__defaults__ = (None,) * len(RecipeAction._fields)  # Sets default values to None
+
+class Conditions(State):
+    def __str__(self): return ' '.join(str(item) for item in self.items())
+
+    def __repr__(self): return str(self)
 
 
 class RecipeScenario:
 
-    def __init__(self, num_components=3, num_recipes=5, rounds=10, ingredients_per_recipe=5,
-                 ingredients='ABCDEFGHIGJKLMNOPQRSTUVWXYZ'):
-        self.possible_components = ingredients[0:num_components]
-        self.recipes = set()  # TODO
-        self.util_per_recipe = ingredients_per_recipe
-        self.rounds = rounds
-
-        self.agents = ['Agent1', 'Agent2']
+    def __init__(self, num_conditions, num_agents, num_valid_recipes, recipe_size):
+        self.num_conditions = num_conditions
+        self.agents = ['Agent' + str(i) for i in range(1, num_agents+1)]
+        self.recipes = self.__make_recipes(num_valid_recipes, recipe_size)
+        self.success_util = 10
+        self.conflict_penalty = 1
 
     def initial_state(self):
-        """
-        Gives the initial state of the scenario.
-            - Empty inventory for each agent
-            - Round
-        """
-        return State({'Inventory 1': ItemCounts({comp: 0 for comp in self.possible_components}),
-                      'Inventory 2': ItemCounts({comp: 0 for comp in self.possible_components}),
-                      'Round': 0,
-                      'Complete': False})
+        """ Gives the initial state of the scenario. """
+        return State({'Conditions': Conditions({i: False for i in range(self.num_conditions)}),
+                      'Complete': False,
+                      })
 
     def actions(self, state):
         """ Returns legal actions in the state. """
-        agent_actions = {agent: [RecipeAction('Wait', None)] for agent in self.agents}
-        if state['Complete'] or state['Round'] >= self.rounds:
+        if state['Complete']:
+            agent_actions = {agent: [] for agent in self.agents}
             return JointActionSpace(agent_actions)
 
-        for agent in self.agents:
-            action_list = [RecipeAction('Prepare', ingredient) for ingredient in self.possible_components]
-
-            if agent == 'Agent2':
-                teammate_inventory = state['Inventory 2']
-                construct_actions = [RecipeAction('Construct', recipe) for recipe in self.recipes
-                                if all(teammate_inventory[ingredient] >= count for ingredient, count in recipe.items())]
-                action_list += construct_actions
-
-                action_list += [RecipeAction('Accept')]
-
-            else:
-                action_list += [RecipeAction('Pass', ingredient) for ingredient in self.possible_components
-                                if state['Inventory 1'][ingredient] > 0]
-
-            agent_actions[agent] += action_list
+        # Otherwise, set actions for each remaining condition.
+        unset_conditions = [key for key, val in state['Conditions'].items() if val is False]
+        agent_actions = {agent: unset_conditions + ['End'] for agent in self.agents}
 
         return JointActionSpace(agent_actions)
 
     def transition(self, state, action):
-        """
-        Cases to handle:
-            - preparing materials and updating inventories
-            - passing items
-            - constructing an object from a recipe
-            - the round
-        """
+        """  """
         new_state_dict = dict(state.copy())
+        action_set = set(action.values())  # all unique actions
 
-        agent1_action = action['Agent1']
-        agent2_action = action['Agent2']
-
-        inventory1 = dict(new_state_dict['Inventory 1'])
-        inventory2 = dict(new_state_dict['Inventory 2'])
-
-        # Agent1 actions
-        if 'Prepare' == agent1_action.act:
-            item = agent1_action.object
-            inventory1[item] += 2
-        elif 'Pass' == agent1_action.act and 'Accept' == agent2_action.act:
-            item = agent1_action.object
-            inventory2[item] += inventory1[item]
-            inventory1[item] = 0
-
-        # Agent2 actions
-        if 'Prepare' == agent2_action.act:
-            item = agent2_action.object
-            inventory2[item] += 1
-        elif 'Construct' == agent2_action.act:
-            recipe = agent2_action.object
-            for item, count in recipe.items():
-                inventory2[item] -= count
+        # Mark completion
+        if 'End' in action_set:
             new_state_dict['Complete'] = True
+            action_set -= {'End'}
 
-        new_state_dict['Inventory 1'] = ItemCounts(inventory1)
-        new_state_dict['Inventory 2'] = ItemCounts(inventory2)
-        new_state_dict['Round'] += 1
+        # Update conditions
+        new_state_dict['Conditions'] = new_state_dict['Conditions'].update({cnd: True for cnd in action_set})
 
-        return Distribution({State(new_state_dict): 1.0})  # all possible outcomes and their associated probabilities
+        # Return all possible outcomes and their associated probabilities.
+        return Distribution({State(new_state_dict): 1.0})
 
     def utility(self, old_state, action, new_state):
-        """
-        Returns the utility of transitioning from one state to another.
-        """
+        """ Returns the utility of transitioning from one state to another. """
         util = 0
-        if 'Construct' == action['Agent2'].act:
-            util += self.util_per_recipe
 
-        agent_actions = action.values()
-        any_pass = any('Pass' == action.act for action in agent_actions)
-        any_accept = any('Accept' == action.act for action in agent_actions)
+        # Calculate conflicts
+        all_set_conditions = [val for val in action.values() if val != 'End']
+        num_overlap = len(all_set_conditions) - len(set(all_set_conditions))
+        util -= num_overlap * self.conflict_penalty
 
-        # penalize for having any pass or accept without having the other
-        if any_pass != any_accept:
-            util -= 1
-
-        if new_state['Complete'] or new_state['Round'] >= self.rounds:
-            # end penalty for extra materials
-            util -= sum(new_state['Inventory 1'].values()) + sum(new_state['Inventory 2'].values())
-
-        return util
+        # if not complete, just return conflict util
+        if not new_state['Complete']:
+            return util
+        else:
+            # TODO - check for valid recipe first
+            return util
 
     def end(self, state):
-        """
-        Returns True if the scenario has reached an end state.
-        """
-        return state['Round'] >= self.rounds or state['Complete']
+        """ Returns True if the scenario has reached an end state. """
+        return state['Complete']
+
+    def __make_recipes(self, num_recipes, recipe_size):
+        return []
