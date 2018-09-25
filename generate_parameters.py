208dee9c23bf629c98454c4770bcad5d11ec4402
Trevor Santarra <trevor@unity3d.com>
2018-09-24 20:36:18 -0700

    Run 10 - test plan size/experience

--- generate_parameters.py
+++ generate_parameters.py
@@ -3,14 +3,15 @@ from collections import OrderedDict
 
 
 def generate_args():
-    vals = OrderedDict([('process_no', [9]),
+    vals = OrderedDict([
+                        ('process_no', [10]),
                         ('scenario_id', [1]),
                         ('heuristic_id', [1]),
-                        ('comm_branch_factor', [3, 5]),
-                        ('comm_iterations', [100]),
-                        ('comm_cost', [0, 2, 5]),
-                        ('plan_iterations', [100, 200]),
-                        ('experience', [0, 10, 50]),
+                        ('comm_branch_factor', [3]),
+                        ('comm_iterations', [100, 250, 1000]),
+                        ('comm_cost', [0]),
+                        ('plan_iterations', [100, 500, 1000]),
+                        ('experience', [0, 100, 1000]),
                         ('trials', [50]),
                         ])
 
