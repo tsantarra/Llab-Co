823cc7dc6b8a6b73f3a2425fe199e5e60e382b7d
Trevor Santarra <trevor@unity3d.com>
2018-11-27 11:01:33 -0800

    Back to 3.7.

--- create_virtual_env.sh
+++ create_virtual_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Load python
-module load python/3.6.5-5
-#module --ignore-cache load python/3.7.0
+#module load python/3.6.5-5
+module --ignore-cache load python/3.7.0
 
 # Activate virtual environment
 rm -rf test_env/*
